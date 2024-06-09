2e4a2edcca355aceb18c67d98c17c4ccaedb2e5b
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-06-09 14:05:28 +0300

    setup database

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView
 from pages.serializers import PostSerializer
 from rest_framework.viewsets import ModelViewSet
 from pages.models import Post
+
+
+
 class HomePageView(TemplateView):
     template_name = "home.html"
 
